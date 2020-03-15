2993a88036a97218901a5575c57cb0b105a2ca20
github username <pratikshya.pp19@gmail.com>
2020-03-14 23:48:22 -0500

    message

---
+++ my-calculator/src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Button {
+    render(){
+        return(<div>
+            <div className="button">
+            <button>1</button>
+            </div>
+            
+        </div>)
+    }
+}
