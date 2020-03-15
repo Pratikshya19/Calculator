26235726f2fb4d4968f2da57b5219f9f5687cad8
github username <pratikshya.pp19@gmail.com>
2020-03-15 14:48:05 -0500

    message

--- my-calculator/src/components/Button.js
+++ my-calculator/src/components/Button.js
@@ -7,19 +7,46 @@ class Button {
             <button>1</button>
             </div>
             <div className="button">
-            <button>1</button>
+            <button>2</button>
             </div>
             <div className="button">
-            <button>1</button>
+            <button>3</button>
             </div>
             <div className="button">
-            <button>1</button>
+            <button>4</button>
             </div>
             <div className="button">
-            <button>1</button>
+            <button>5</button>
             </div>
             <div className="button">
-            <button>1</button>
+            <button>6</button>
+            </div>
+            <div className="button">
+            <button>7</button>
+            </div>
+            <div className="button">
+            <button>8</button>
+            </div>
+            <div className="button">
+            <button>9</button>
+            </div>
+            <div className="button">
+            <button>0</button>
+            </div>
+            <div className="button">
+            <button>/</button>
+            </div>
+            <div className="button">
+            <button>+</button>
+            </div>
+            <div className="button">
+            <button>-</button>
+            </div>
+            <div className="button">
+            <button>*</button>
+            </div>
+            <div className="button">
+            <button>.</button>
             </div>
         </div>)
     }
