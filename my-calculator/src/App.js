0dd576b23f52c48c365f96dfdb5b8490d97b64eb
github username <pratikshya.pp19@gmail.com>
2020-03-16 23:40:28 -0500

    Monday

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,13 +1,67 @@
 import React from "react";      
 import './App.css';
 import Button from "./components/Button"
+import Input from "./components/Input"
+import ClearButton from "./components/ClearButton"
 
 class App extends React.Component { 
+  constructor(props){
+    super(props)
+      this.state = {
+        input: "",
+        previousNumber: "",
+        operator: ""
+    }
+  }
+addZeroToInput = val => {
+  //if this.state.input is not empty then add zero
+}
+  addToInput = val => {
+    this.setState({
+      input: this.state.input + val
+    })
+  }
+
   render(){
-    return(<div>
-      <h1>HELLO REACT</h1>
-    
-    
+    return(
+    <div className="App">
+      <div className="cal-wrapper">
+      <div className="row">
+        <Input>
+        {this.state.input}
+        </Input>
+        </div>
+      <div className="row">
+        <Button handleClick={this.addToInput}>7</Button>
+        <Button handleClick={this.addToInput}>8</Button>
+        <Button handleClick={this.addToInput}>9</Button>
+        <Button handleClick={this.addToInput}>/</Button>
+        </div>
+        <div className="row">
+        <Button handleClick={this.addToInput}>4</Button>
+        <Button handleClick={this.addToInput}>5</Button>
+        <Button handleClick={this.addToInput}>6</Button>
+        <Button handleClick={this.addToInput}>*</Button>
+        </div>
+        <div className="row">
+        <Button handleClick={this.addToInput}>1</Button>
+        <Button handleClick={this.addToInput}>2</Button>
+        <Button handleClick={this.addToInput}>3</Button>
+        <Button handleClick={this.addToInput}>+</Button>
+        </div>
+        <div className="row">
+        <Button handleClick={this.addToInput}>.</Button>
+        <Button handleClick={this.addZeroToInput}>0</Button>
+        <Button handleClick={this.addToInput}>=</Button>
+        <Button handleClick={this.addToInput}>-</Button>
+        </div>
+        <div className="row">
+          <ClearButton>
+            Clear
+          </ClearButton>
+        </div>
+        
+        </div>
     </div>)}
 
   
--- my-calculator/src/components/Button.js
+++ my-calculator/src/components/Button.js
@@ -1,53 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Button.css'
 
-class Button {
+
+class Button extends Component {
+    isOperator = val => { 
+        return !isNaN(val) || val === "." || val === "=";
+    }
+
     render(){
-        return(<div>
-            <div className="button">
-            <button>1</button>
-            </div>
-            <div className="button">
-            <button>2</button>
-            </div>
-            <div className="button">
-            <button>3</button>
-            </div>
-            <div className="button">
-            <button>4</button>
-            </div>
-            <div className="button">
-            <button>5</button>
-            </div>
-            <div className="button">
-            <button>6</button>
-            </div>
-            <div className="button">
-            <button>7</button>
-            </div>
-            <div className="button">
-            <button>8</button>
-            </div>
-            <div className="button">
-            <button>9</button>
-            </div>
-            <div className="button">
-            <button>0</button>
-            </div>
-            <div className="button">
-            <button>/</button>
-            </div>
-            <div className="button">
-            <button>+</button>
-            </div>
-            <div className="button">
-            <button>-</button>
-            </div>
-            <div className="button">
-            <button>*</button>
-            </div>
-            <div className="button">
-            <button>.</button>
-            </div>
+        return(
+        <div className={`button ${this.isOperator(this.props.children) ? "" : "operator"}`}
+        onClick={()=> this.props.handleClick(this.props.children)}
+        >
+           {this.props.children}
         </div>)
     }
 }
+export default Button;
